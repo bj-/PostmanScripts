1584f5f0e5a092b22236b5d25bfb3e0361de6750
bj- <bmw518@bk.ru>
2024-07-17 14:03:54 +0300

    Create After Responce (common).js

---
+++ After Responce (common).js
@@ -0,0 +1,17 @@
+utils = {
+  basictests: function() {
+    basictests();
+  },
+  test_graphql: function(path, exp, type, silent) {
+    test_graphql (path, exp, type, silent);
+  },
+  check: function(parameter, exp, type, silent) {
+    check(parameter, exp, type, silent);
+  },
+  setvar: function(varName, path, space) {
+    setvar(varName, path, space);
+  },
+  randomString: function(length=1) {
+    randomString(length)
+  }
+};
